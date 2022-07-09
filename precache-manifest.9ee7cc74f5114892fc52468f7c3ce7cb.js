e691868663fbb6f3dd3ec1ac3e1b7e5fb1a97e69
soni2 <soni2gf@gmail.com>
2022-07-09 18:44:26 -0400

    Updates

---
+++ precache-manifest.9ee7cc74f5114892fc52468f7c3ce7cb.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "86d878c9433f785948ff",
+    "url": "/taskmng2/static/css/main.887846bd.chunk.css"
+  },
+  {
+    "revision": "86d878c9433f785948ff",
+    "url": "/taskmng2/static/js/main.86d878c9.chunk.js"
+  },
+  {
+    "revision": "d655ce1cf88952e278a0",
+    "url": "/taskmng2/static/js/1.d655ce1c.chunk.js"
+  },
+  {
+    "revision": "42097e413bb1975685ff",
+    "url": "/taskmng2/static/js/runtime~main.42097e41.js"
+  },
+  {
+    "revision": "4fa9364e4ef0aacca9f16eca2fc92a7a",
+    "url": "/taskmng2/static/media/theme.4fa9364e.svg"
+  },
+  {
+    "revision": "6ff248508018868a7a0fc149dac8e842",
+    "url": "/taskmng2/static/media/check-color.6ff24850.svg"
+  },
+  {
+    "revision": "33c39d441eb5ed79b07aceb963c380bb",
+    "url": "/taskmng2/static/media/check-grey.33c39d44.svg"
+  },
+  {
+    "revision": "bb47648a2c8a9834426a1736acdc5d1b",
+    "url": "/taskmng2/static/media/trash-red.bb47648a.svg"
+  },
+  {
+    "revision": "d80cebf98994d550bfe974d0a6cb83e0",
+    "url": "/taskmng2/static/media/trash.d80cebf9.svg"
+  },
+  {
+    "revision": "faabd1bb74edfcb80dbc3db37bb2422c",
+    "url": "/taskmng2/static/media/addTodo.faabd1bb.svg"
+  },
+  {
+    "revision": "a83ee8847bccf683803f040cd60ff1a9",
+    "url": "/taskmng2/index.html"
+  }
+];
