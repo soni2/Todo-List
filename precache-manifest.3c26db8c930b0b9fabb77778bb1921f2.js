6832b07461eaa007694b7d39d571ba5a2ac00ac3
soni2 <soni2gf@gmail.com>
2022-07-10 14:20:32 -0400

    Updates

--- precache-manifest.3c26db8c930b0b9fabb77778bb1921f2.js
+++ precache-manifest.3c26db8c930b0b9fabb77778bb1921f2.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "3247450f75c8dba1efb2",
+    "revision": "42c4f0c2aaf074e3295d",
     "url": "/taskmng2/static/css/main.41431935.chunk.css"
   },
   {
-    "revision": "3247450f75c8dba1efb2",
-    "url": "/taskmng2/static/js/main.3247450f.chunk.js"
+    "revision": "42c4f0c2aaf074e3295d",
+    "url": "/taskmng2/static/js/main.42c4f0c2.chunk.js"
   },
   {
     "revision": "bee36d96ecf68392c5e3",
@@ -16,20 +16,20 @@ self.__precacheManifest = [
     "url": "/taskmng2/static/js/runtime~main.42097e41.js"
   },
   {
-    "revision": "33c39d441eb5ed79b07aceb963c380bb",
-    "url": "/taskmng2/static/media/check-grey.33c39d44.svg"
-  },
-  {
     "revision": "6ff248508018868a7a0fc149dac8e842",
     "url": "/taskmng2/static/media/check-color.6ff24850.svg"
   },
   {
-    "revision": "bb47648a2c8a9834426a1736acdc5d1b",
-    "url": "/taskmng2/static/media/trash-red.bb47648a.svg"
+    "revision": "33c39d441eb5ed79b07aceb963c380bb",
+    "url": "/taskmng2/static/media/check-grey.33c39d44.svg"
   },
   {
     "revision": "d80cebf98994d550bfe974d0a6cb83e0",
     "url": "/taskmng2/static/media/trash.d80cebf9.svg"
+  },
+  {
+    "revision": "bb47648a2c8a9834426a1736acdc5d1b",
+    "url": "/taskmng2/static/media/trash-red.bb47648a.svg"
   },
   {
     "revision": "faabd1bb74edfcb80dbc3db37bb2422c",
@@ -40,7 +40,7 @@ self.__precacheManifest = [
     "url": "/taskmng2/static/media/brocha.1798e731.svg"
   },
   {
-    "revision": "c8ae818e2f589975ed049e670ae7dc20",
+    "revision": "6c573c60e647ccde4514418481dc6648",
     "url": "/taskmng2/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/taskmng2/precache-manifest.fbea4cf0e0c9929950e42864cef680ae.js"
+  "/taskmng2/precache-manifest.3c26db8c930b0b9fabb77778bb1921f2.js"
 );
 
 workbox.clientsClaim();
